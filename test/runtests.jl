9456a7a69884485d145c7067e46891c76d45183a
Morten Piibeleht <morten.piibeleht@gmail.com>
2022-08-12 16:45:22 +1200

    test: check for method ambiguities

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,8 @@ using MarkdownAST
 using Test
 
 @testset "MarkdownAST" begin
+    @test isempty(Test.detect_ambiguities(MarkdownAST; recursive=true))
+
     include("markdown.jl")
     include("node.jl")
     include("invalidast.jl")
